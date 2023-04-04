ca1816e16154916145e5cb74172a07cc9c9c10fa
nima <nmoosavi@live.com>
2023-04-04 20:52:42 +0330

    Polished

--- src/main/java/com/snap/linkshortener/kgs/repository/usedkeys/UsedKeys.java
+++ src/main/java/com/snap/linkshortener/kgs/repository/usedkeys/UsedKeys.java
@@ -1,13 +1,10 @@
 package com.snap.linkshortener.kgs.repository.usedkeys;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-
-import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Table(name = "USED_KEYS")
